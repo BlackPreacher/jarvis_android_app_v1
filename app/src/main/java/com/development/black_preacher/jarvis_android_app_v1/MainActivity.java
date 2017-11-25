b2d0195c30ff1772ec71a6da6fa5b6430cd52715
BlackPreacher <megasven9@gmx.de>
2017-11-25 12:27:43 +0100

    Initial commit

---
+++ app/src/main/java/com/development/black_preacher/jarvis_android_app_v1/MainActivity.java
@@ -0,0 +1,13 @@
+package com.development.black_preacher.jarvis_android_app_v1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
